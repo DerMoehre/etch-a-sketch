fca955311cf010e01b7d3393ab9d6d4b43cf822b
DerMoehre <94791958+DerMoehre@users.noreply.github.com>
2024-02-05 21:16:08 +0100

    basic block logic

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let createGrid = () => {
+    const sketchArea = document.querySelector('.sketch_area');
+    amount = 50;
+
+    for (let i = 0; i < amount; i++) {
+        let box = document.createElement('div');
+        box.classList.add('row');
+        sketchArea.appendChild(box);
+
+        for (let j = 0; j < amount; j++) {
+            let sketchBox = document.createElement('div');
+            sketchBox.classList.add('box');
+            box.appendChild(sketchBox);
+        }
+    }
+}
+
+createGrid();
