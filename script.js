352c9dcdcfd431a209ae28844e144f92090112a5
DerMoehre <94791958+DerMoehre@users.noreply.github.com>
2024-02-11 09:39:44 +0100

    fading coloring added

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 
-let createGrid = () => {
+let emptyGrid = () => {
+    let sketchArea = document.querySelector('.sketch_area');
+
+    sketchArea.innerHTML = '';
+}
+
+let createGrid = (amount = 4) => {
     const sketchArea = document.querySelector('.sketch_area');
-    amount = 50;
 
     for (let i = 0; i < amount; i++) {
         let box = document.createElement('div');
@@ -19,9 +24,46 @@ let createGrid = () => {
 createGrid();
 
 let boxLayout = document.querySelectorAll('.box');
+let userColor = document.querySelector('.btn_color').value;
+let loadBtn = document.querySelector('.btn_load');
+let updateColor = document.querySelector('.btn_choose_color');
 
-boxLayout.forEach((box) => {
-    box.addEventListener('mouseleave', () => {
-        box.setAttribute('style', 'background: black');
-    });
+updateColor.addEventListener('click', () => {
+    userColor = document.querySelector('.btn_color').value;
+    coloring(userColor);
 });
+
+loadBtn.addEventListener('click', () => {
+    emptyGrid();
+    let user_amount = document.querySelector('.amount_box').value;
+    if (user_amount < 1) {
+        createGrid();
+    } else if (user_amount > 100) {
+        createGrid(100);
+    } else {
+        createGrid(user_amount);
+    }
+    boxLayout = document.querySelectorAll('.box');
+    userColor = document.querySelector('.btn_color').value;
+    coloring(userColor);
+});
+
+let getOpacity = (box) => {
+    let currentOpacity = Number(window.getComputedStyle(box).getPropertyValue("opacity"));
+    let updatedOpacity = currentOpacity + 0.1;
+    return updatedOpacity;
+}
+
+let coloring = (color) => {
+    boxLayout.forEach((box) => {
+        box.addEventListener('mouseover', () => {
+            let currentOpacity = getOpacity(box);
+            box.setAttribute('style', `background: ${color}; opacity: ${currentOpacity};`);
+        });
+    });
+}
+
+coloring(userColor);
+
+
+
